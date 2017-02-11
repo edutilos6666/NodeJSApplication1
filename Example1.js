760665b9f6bd2603092ca5976853ffb8beddc755
edutilos666 <edutilosaghayev@gmail.com>
2017-02-11 23:56:42 +0100

    m

--- Example1.js
+++ Example1.js
@@ -1,1 +1,10 @@
-console.log("hello world");
+var http = require('http');
+
+http.createServer(function(req, resp){
+    resp.writeHead(200, {'Content-Type': 'text/html'});
+    resp.end("hello world by pako");
+}).listen(8080);
+
+
+
+console.log("server is listening on port 8080");
